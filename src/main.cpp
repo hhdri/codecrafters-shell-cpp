45cbaa04b6d4237d75f0573132ab4685538b7818
Majid Hajiheidari <majidhhdri@gmail.com>
2025-11-12 23:28:48 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,18 @@ int main() {
       int exitStatus;
       std::cin >> exitStatus;
       return exitStatus;
-    } else {
+    }
+    else if (command == "echo") {
+      std::string echoResult;
+      std:getline(std::cin, echoResult);
+      // std::string echoPiece;
+      // do {
+      //   std::cin >> echoPiece;
+      //   echoResult += " " + echoPiece;
+      // } while (echoPiece != "\n");
+      std::cout << echoResult << std::endl;
+    }
+     else {
       std::cout << command << ": command not found" << std::endl;
     }
   }
