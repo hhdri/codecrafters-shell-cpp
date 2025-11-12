e2907308fea1579cdcccb79d5effd954fc5ab3bd
Majid Hajiheidari <majidhhdri@gmail.com>
2025-11-13 00:02:11 +0100

    Read PATH env var

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,27 @@ int main() {
       std::cin >> arg;
       if (arg == "exit" || arg == "echo" || arg == "type")
         std::cout << arg << " is a shell builtin\n";
-      else
-        std::cout << arg << ": not found\n";
+      std::string path = std::getenv("PATH");
+      std::cout << path << '\n';
+      std::vector<std::string> pathParts;
+      int startIdx = 0, length = 0;
+      for (int i = 0; i < path.length(); i++) {
+        char curr = path[i];
+        if (curr != ':') {
+          length++;
+          if (i != path.length() - 1)
+            continue;
+        }
+        if (length != 0) {
+          pathParts.push_back(path.substr(startIdx, length));
+          startIdx = i + 1;
+          length = 0;
+        }
+      }
+      for (auto pathPart: pathParts)
+        std::cout << pathPart << '\n';
+      //else
+      //  std::cout << arg << ": not found\n";
     }
      else {
       std::cout << command << ": command not found" << std::endl;
